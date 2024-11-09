9c548e104e4e79380b2b859a15d0274a7b59f6ff
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-11-09 01:55:55 +0100

    fixed

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('admin/', admin_view.admin_view, name='admin_view'),
-    path('librarian/', librarian_view.librarian_view, name='librarian_view'),
-    path('member/', member_view.member_view, name='member_view'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,6 +7,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+from django.http import HttpResponseForbidden
 
 
 # Create your views here.
@@ -46,3 +49,29 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+# Check if the user has the 'Librarian' role
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+# Check if the user has the 'Member' role
+def is_member(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Admin view - only accessible by 'Admin' users
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view - only accessible by 'Librarian' users
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view - only accessible by 'Member' users
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
