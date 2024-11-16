ab83bf5d44827e96a056f202a50a2c62681c1ea7
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-11-16 16:16:32 +0100

    fix

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser, BaseUserManager
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
 
     def __str__(self):
             return f"{self.title} by {self.author} ({self.publication_year})"
