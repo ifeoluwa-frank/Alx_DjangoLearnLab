a9f30a86d65b98738a69508231b579f95c6b65e9
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-11-16 16:18:48 +0100

    fix

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -10,13 +10,33 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    isbn = models.CharField(max_length=13, default='Unknown')  # default value added here
+    summary = models.TextField()
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username must be set")
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(username, password, **extra_fields)
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-    def __str__(self):
-        return self.username
+    objects = CustomUserManager()
+
+# SomeModel linked to CustomUser
+class SomeModel(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
-            return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.title
