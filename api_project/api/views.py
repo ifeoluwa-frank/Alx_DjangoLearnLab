93fbeffbc7c5f7a6c7ccdb8c0a0118775292f8aa
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-11-23 09:57:46 +0100

    permission

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -17,3 +18,4 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
