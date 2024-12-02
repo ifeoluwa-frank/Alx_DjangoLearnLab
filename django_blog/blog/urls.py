99a483d3c9f87550565da667bb71a7a9054716f8
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-12-02 22:37:19 +0100

    fix

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -27,5 +27,7 @@ urlpatterns = [
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     path('search/', search_posts, name='search-posts'),
-    path('tags/<str:tag_name>/', posts_by_tag, name='posts-by-tag'),
+    #path('tags/<str:tag_name>/', posts_by_tag, name='posts-by-tag'),
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='post_by_tag'),
+
 ]
