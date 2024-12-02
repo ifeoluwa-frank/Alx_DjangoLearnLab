5613abf914aae11fd0998ef05bf09e4ce2e14f09
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-12-02 21:32:39 +0100

    fix

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -74,12 +74,19 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',  # Replace with your database name
+        'USER': 'root',     # Replace with your database username
+        'PASSWORD': 'harare',  # Replace with your database password
+        'HOST': 'localhost',       # Database host (default: localhost)
+        'PORT': '3306',            # Database port (default: 3306)
     }
 }
+
 
 
 # Password validation
