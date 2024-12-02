edc83ce60cf98ca98f4130b3054d32f6559763a9
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-12-02 21:44:20 +0100

    Blog user auth

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 # Create your views here.
+
+# Registration view
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "blog/register.html", {"form": form})
+
+# Login view
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = AuthenticationForm()
+    return render(request, "blog/login.html", {"form": form})
+
+# Logout view
+def user_logout(request):
+    logout(request)
+    return redirect("login")
+
+# Profile view
+@login_required
+def profile(request):
+    return render(request, "blog/profile.html", {"user": request.user})
