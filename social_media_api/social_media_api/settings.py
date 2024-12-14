cb26594c7988c08bef3c7ea9fb5b61618559661c
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-12-14 14:43:19 +0100

    fix

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -134,3 +134,13 @@ X_FRAME_OPTIONS = 'DENY'
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_SSL_REDIRECT = True  # Set to True if you want to redirect HTTP to HTTPS
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your_db_host',
+        'PORT': 'your_db_port',
+    }
+}
