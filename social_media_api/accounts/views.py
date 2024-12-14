02d16c7c3b28d5a224ddf1bf14612f15c9a8152d
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-12-14 13:36:51 +0100

    0

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, UserSerializer
+
+# Create your views here.
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({
+            'token': token.key,
+            'user_id': token.user_id,
+            'username': token.user.username,
+        })
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
